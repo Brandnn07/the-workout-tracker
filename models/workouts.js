073146f10c04962e51eff7bf44ccd87345d35dd2
brandnn07 <brandcarter4@gmail.com>
2021-04-05 11:06:10 -0400

    made changes to display dashboard

--- models/workouts.js
+++ models/workouts.js
@@ -1,41 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: new Date()
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        required: true
-      },
-      name: {
-        type: String,
-        required: true
-      },
-      duration: {
-        type: Number,
-        required: true
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-      distance: {
-        type: Number,
-      }
-    }
-  ]
-})
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            name: {
+                type: String,
+                trim: true
+            },
+            type: {
+                type: String,
+                trim: true
+            },
+            weight: {
+               type: Number 
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ],
+});
 
-const Workout = mongoose.model("Workout", workoutSchema)
+const Workout = mongoose.model('Workout', workoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require('morgan');
 const path = require('path');
 
 
 const PORT = process.env.PORT || 3000
 
+const db = require('./models');
+
 const app = express();
+
+app.use(logger('dev'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,12 +19,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/homeroutes.js"));
+app.use(require("./routes"));
+
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
