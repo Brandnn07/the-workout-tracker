1d6d6a804eac1a665601b169207454c28173aee6
brandnn07 <brandcarter4@gmail.com>
2021-04-05 11:12:39 -0400

    code is functional, final touches

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/homeroutes.js"));
 
 
 app.listen(PORT, () => {
