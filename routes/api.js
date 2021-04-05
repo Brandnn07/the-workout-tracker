b7c9ebdcf83be18011bd6c34065d43e452e79dd5
brandnn07 <brandcarter4@gmail.com>
2021-04-05 11:10:19 -0400

    trying to remove undefined error

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,88 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { db } = require("../models/workouts.js");
+const Workout = require("../models/workouts.js");
 
-router.use('/workout', require('./workout'));
+
+
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([{
+    $addFields: {
+      totalDuration: { $sum: "$exercises.duration" }
+    }
+  }])
+    .sort({ _id: 1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+});
+
+router.post('/api/workouts', ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+});
+
+router.post('/api/workouts/bulk', ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id,
+    {
+      $push: {
+        exercises: [req.body]
+      }
+    },
+    { new: true })
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+});
+
+//workouts range?????????
+router.get('/api/workouts/range', (req, res) => {
+  Workout.aggregate([{
+    $addFields: {
+      totalDuration: { $sum: "$exercises.duration" }
+    }
+  }])
+    .sort({ day: -1 })
+    .then(dbWorkout => {
+      function compare(a, b) {
+        const dayA = a.day;
+        const dayB = b.day;
+
+        let comparison = 0;
+        if (dayA > dayB) {
+          comparison = 1;
+        } else if (dayA < dayB) {
+          comparison = -1;
+        }
+        return comparison;
+      }
+      dbWorkout.length = 7;
+      dbWorkout.sort(compare)
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+})
 
 module.exports = router;
