a714025346c5fd90a729921d12f26c9e5c3e0533
brandnn07 <brandcarter4@gmail.com>
2021-04-05 17:46:48 -0400

    code is finished, deploying to heroku

--- routes/api.js
+++ routes/api.js
@@ -2,65 +2,58 @@ const router = require("express").Router();
 const Workout = require("../models/workouts.js");
 const { db } = require("../models/workouts.js");
 
-
-
-router.get('/api/workouts', (req, res) => {
-  Workout.aggregate([{
-    $addFields: {
-      totalDuration: { $sum: "$exercises.duration" }
-    }
-  }])
-    .sort({ _id: 1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    })
-});
-
-router.post('/api/workouts', ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    })
-});
-
-router.post('/api/workouts/bulk', ({ body }, res) => {
-  Workout.insertMany(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout)
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    })
-});
-
-router.put('/api/workouts/:id', (req, res) => {
-  Workout.findByIdAndUpdate(req.params.id,
-    {
-      $push: {
-        exercises: [req.body]
+router.get('/range', (req, res) => {
+  db.Workout.aggregate([{
+      $addFields: {
+          totalDuration: { $sum: '$exercises.duration'}
       }
-    },
-    { new: true })
-    .then(dbWorkout => {
-      res.json(dbWorkout)
-    })
-    .catch(err => {
+  }])
+  .then(dbWorkout => {
+      res.json(dbWorkout);
+  })
+  .catch(err => {
       res.status(400).json(err);
-    })
+  });
 });
 
-router.get('/api/workouts/range', (req, res) => {
-  Workout.aggregate([{
-    $addFields: {
-      totalDuration: { $sum: "$exercises.duration" }
-    }
-  }])
-})
+router.post('/', ({ body }, res) => {
+  db.Workout.create(body)
+  .then(dbWorkout => {
+      res.json(dbWorkout);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  });
+});
 
+router.put('/:id', ( {body, params }, res) => {
+  db.Workout.findByIdAndUpdate({
+      _id: params.id
+  },
+  {
+      $push: {exercises: body }
+  },
+  {
+      new: true})
+  .then(dbWorkout => {
+      res.json(dbWorkout);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  });
+});
+
+router.get('/', (req, res) => {
+  db.Workout.aggregate([{
+      $addFields: {
+          totalDuration: { $sum: '$exercises.duration'}
+      }
+  }])
+  .then(dbWorkout => {
+      res.json(dbWorkout);
+  })
+  .catch(err => {
+      res.status(400).json(err);
+  });
+});
 module.exports = router;
