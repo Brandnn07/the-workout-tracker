bf064129435d231ff5487ff5752f552d06b1d1ef
brandnn07 <brandcarter4@gmail.com>
2021-04-05 18:21:39 -0400

    potential fix

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require('morgan');
 const path = require('path');
-
 
 const PORT = process.env.PORT || 3000
 
-const db = require('./models');
-
 const app = express();
 
-app.use(logger('dev'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,8 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true
 });
 
 app.use(require("./routes/api.js"));
