6f3cebe841a413473b9b9f87c7162cb63b06b09d
Brandnn07 <brandcarter4@gmail.com>
2021-04-05 19:29:12 -0400

    final commit

--- routes/api.js
+++ routes/api.js
@@ -18,6 +18,20 @@ router.get('/api/workouts', (req, res) => {
     })
 });
 
+router.get('/api/workouts/range', (req, res) => {
+  Workout.aggregate([{
+    $addFields: {
+        totalDuration: { $sum: '$exercises.duration'}
+    }
+}])
+.then(dbWorkout => {
+    res.json(dbWorkout);
+})
+.catch(err => {
+    res.status(400).json(err);
+});
+})
+
 router.post('/api/workouts', ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout => {
@@ -53,19 +67,5 @@ router.put('/api/workouts/:id', (req, res) => {
       res.status(400).json(err);
     })
 });
-
-router.get('/api/workouts/range', (req, res) => {
-  Workout.aggregate([{
-    $addFields: {
-        totalDuration: { $sum: '$exercises.duration'}
-    }
-}])
-.then(dbWorkout => {
-    res.json(dbWorkout);
-})
-.catch(err => {
-    res.status(400).json(err);
-});
-})
 
 module.exports = router;
